8295b89976e21dbec5877b218c649ae632fe6370
Sorcha Bowler <saoili@gmail.com>
2014-01-02 18:17:55 +0000

    refactor functional test (to make it a bit less stupid), still in chapter 5

--- functional_tests.py
+++ functional_tests.py
@@ -40,9 +40,7 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -51,14 +49,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly' ,
-            [row.text for row in rows]
-        )
-
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+  
         # Edith wonders whether the site will remember her list. Then she notices
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
